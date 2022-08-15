24f6068ceda8d082ba1aa26e2d8bf21cd94ff1fd
SASHAFAME <sadsparrow@mail.ru>
2022-08-15 18:37:10 +0300

    refactor: audio player

--- js/playList.js
+++ js/playList.js
@@ -1,32 +1,32 @@
 const playList = [
     {
         title: 'Priora',
-        src: '../assets/sounds/priora.mp3',
+        src: './assets/sounds/priora.mp3',
         duration: '01:15'
     },
     {
         title: 'ROCKET x SEREBRO - Первый Сладкий Инкассатор',
-        src: '../assets/sounds/Inkasssator.mp3',
+        src: './assets/sounds/Inkasssator.mp3',
         duration: '03:31'
     },
     {
     title: 'Babyjon - hype',
-    src: '../assets/sounds/babyjon-haipovat.mp3',
+    src: './assets/sounds/babyjon-haipovat.mp3',
     duration: '01:09'
     },
     {
         title: 'Gucci Bag',
-        src: '../assets/sounds/Ocean96-GucciBag.mp3',
+        src: './assets/sounds/Ocean96-GucciBag.mp3',
         duration: '01:51'
     },
     {
         title: 'Aqua Caelestis',
-        src: '../assets/sounds/AquaCaelestis.mp3',
+        src: './assets/sounds/AquaCaelestis.mp3',
         duration: '00:58'
     },
     {
         title: 'River Flows In You',
-        src: '../assets/sounds/RiverFlowsInYou.mp3',
+        src: './assets/sounds/RiverFlowsInYou.mp3',
         duration: '03:50'
     }
 ]
