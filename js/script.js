5b0bb34f8e2b72250d03afb0e55fd3a3ca105a91
SASHAFAME <sadsparrow@mail.ru>
2022-08-15 17:12:17 +0300

    feat: add player

--- js/script.js
+++ js/script.js
@@ -142,3 +142,158 @@ function getSlidePrev() {
 slidePrev.addEventListener('click', getSlidePrev)
 
 // SLIDER END
+
+// WEATHER START
+const city = document.querySelector('.city');
+async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=f35bea9bca3e8ae10cf9816c3942ee16&units=metric`
+    const res = await fetch(url);
+    const data = await res.json();
+
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`)
+    temperature.textContent = `${data.main.temp} °C`
+    weatherDescription.textContent = data.weather[0].description;
+}
+getWeather()
+
+let weatherCity = document.querySelector('.city')
+function setWeatherLocalStorage() {
+
+    localStorage.setItem('weather', weatherCity.value);
+}
+window.addEventListener('beforeunload', setWeatherLocalStorage)
+
+function getWeatherLocalStorage() {
+    if(localStorage.getItem('weather')) {
+        weatherCity.value = localStorage.getItem('weather');
+    }
+}
+window.addEventListener('load', getWeatherLocalStorage)
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+
+
+
+city.addEventListener('change', getWeather)
+
+
+  
+  document.addEventListener('DOMContentLoaded', getWeather);
+
+//   WEATHER END
+
+// QUOTE OF THE DAY START
+
+const quote = document.querySelector('.quote')
+const author = document.querySelector('.author')
+
+async function getQuotes() {
+    function getRandomQuote(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    let quoteNum = getRandomQuote(0, 7).toString()
+
+    const quotes = 'data.json';
+    const res = await fetch(quotes);
+    const data = await res.json();
+    author.textContent = data[quoteNum].author
+    quote.textContent = data[quoteNum].text
+}
+getQuotes()
+
+
+const changerQuote = document.querySelector('.change-quote')
+
+changerQuote.addEventListener('click', getQuotes)
+
+// QUOTE OF THE DAY END
+
+// AUDIO START
+
+let isPlay = false;
+
+const audio = document.querySelector('audio');
+const playPrev = document.querySelector('.play-prev')
+const playNext = document.querySelector('.play-next')
+const play = document.querySelector('.play')
+
+function playAudio() {
+
+    if (isPlay === false) {
+    audio.currentTime = 0;
+    audio.play();
+    isPlay = true;
+    play.classList.add('pause')
+
+    console.log(isPlay)
+} else if (isPlay === true) {
+    audio.pause();
+    isPlay = false;
+    play.classList.remove('pause')
+
+    console.log(isPlay)
+
+}}
+
+play.addEventListener('click', playAudio)
+
+let playNum = 0
+
+function playNextSound() {
+    playNum = playNum+1
+    if (playNum > 5) {
+        playNum = 5
+    }
+    audio.src = playList[playNum].src;
+    console.log(playNum)
+
+    if (isPlay === true) {
+        audio.pause();
+        audio.play();
+    }
+    isPlay = true;
+    play.classList.add('pause')
+    audio.play();
+}
+
+function playPrevSound() {
+    playNum = playNum-1
+    if (playNum < 0) {
+        playNum = 0
+    }
+    audio.src = playList[playNum].src;
+    console.log(playNum)
+
+    if (isPlay === true) {
+        audio.pause();
+        audio.play();
+    }
+    isPlay = true;
+    play.classList.add('pause')
+    audio.play();
+}
+
+playNext.addEventListener('click', playNextSound)
+playPrev.addEventListener('click', playPrevSound)
+
+import playList from './playList.js';
+console.log(playList);
+
+
+// const li = document.createElement('li');
+// const playListUl = document.querySelector('.play-list')
+// li.classList.add('play-item')
+
+// async function playListFunc() {
+//     const listAudio = 'playList.js'
+//     const res = fetch(listAudio)
+//     const data = await res.json()
+//     playListUl.textContent = data[playNum].title;
+// }
+// playListFunc()
+// AUDIO END
